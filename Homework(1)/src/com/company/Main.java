02676be0957865e3f23ae181174d3683f3537522
Almanbet Totoev <almanbet.totoev@mail.ru>
2021-04-24 01:19:56 +0600

    homework1

---
+++ Homework(1)/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        System.out.println("Задача А: ");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите три целых числа: ");
+//        int firstNumber = sc.nextInt();
+//        int secondNumber = sc.nextInt();
+//        int thirdNumber = sc.nextInt();
+//        int sum = firstNumber + secondNumber + thirdNumber;
+//        int composition = firstNumber * secondNumber * thirdNumber;
+//        float arMean = sum/3f;
+//        System.out.println(firstNumber + "+" + secondNumber + "+" + thirdNumber + "=" + sum);
+//        System.out.println(firstNumber + "*" + secondNumber + "*" + thirdNumber + "=" + composition);
+//        System.out.printf("(" + firstNumber + "+" + secondNumber + "+" + thirdNumber + ")" + "/3=" + "%.3f",arMean);
+
+
+
+
+
+//        System.out.println("Задача В: ");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите градусы в цельсиях: ");
+//        double celsius = sc.nextDouble();
+//        double farenheit = (celsius * 9/5) + 32;
+//        System.out.printf("Градусы в фаренгейтах = " + "%.2f",farenheit);
+
+
+
+
+//        System.out.println("Задача С: ");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите трехзначное число: ");
+//        int a = sc.nextInt();
+//        System.out.printf("%d, %d, %d ", a/100, a/10%10, a%10);
+    }
+}
+
