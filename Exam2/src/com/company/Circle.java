f89a40d7fa267032796f8de2f0e119bd7e8b4a44
Almanbet Totoev <almanbet.totoev@mail.ru>
2021-05-21 21:58:11 +0600

    Task1

---
+++ Exam2/src/com/company/Circle.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Circle {
+    private double radius;
+    private static final float PI = 3.14f;
+    protected static int circlesMultiplesThree;
+
+    protected double findArea(){
+        double y = PI * Math.pow(radius, 2);
+        return y;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public static float getPI() {
+        return PI;
+    }
+
+    public static int getCirclesMultiplesThree() {
+        return circlesMultiplesThree;
+    }
+
+    public static void setCirclesMultiplesThree(int circlesMultiplesThree) {
+        Circle.circlesMultiplesThree = circlesMultiplesThree;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
